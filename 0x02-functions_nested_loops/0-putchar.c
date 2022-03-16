913b7eb03a001c94a339a86661457d112ba72d0b
tomhiwet <tomhiwet@gmail.com>
2022-03-16 01:50:34 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+	    char ch = 'a';
+
+	    	for (ch = 'a'; ch <= 'z'; ch++)
+				{
+					_putchar(ch);
+								}
+			_putchar('\n');
+
+	        return (0);
+}
