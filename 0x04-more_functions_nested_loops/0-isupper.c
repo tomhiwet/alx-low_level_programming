b7c4ab7a7fc80c12bbda140aeed91682b5b35737
tomhiwet <tomhiwet@gmail.com>
2022-03-18 03:37:05 -0700

    more loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,17 @@
-nclude "holberton.h"
+#include "holberton.h"
 
 /**
- *  * _isupper - function that checks for uppercase character.
- *   *
- *    * @c: uppercase character.
- *     *
- *      * Return: 1 if c is uppercase, 0 otherwise.
- *      */
+ * _isupper - define if a character is uppercase
+ *
+ * Description: Prints the alphabet with _putchar
+ * @c: charater
+ * Return: void
+ */
 
 int _isupper(int c)
 {
-		if (c >= 'A' && c <= 'Z')
-				{
-							return (1);
-								}
-			else
-						return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
