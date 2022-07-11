cddfec1791c3aa959bcb34687fb1b5c1f53f168d
tomhiwet <tomhiwet@gmail.com>
2022-07-11 06:31:32 -0700

    more functions

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,14 @@
-#include "holberton.h"
+#include "main.h"
 
+/**
+ * _isdigit - Check for digits (0 through 9)
+ * @c: int type param
+ * Return: 1 if digit, else 0
+ */
 int _isdigit(int c)
 {
-		if (c >= '0' && c <= '9')
-			return (1);
-		else
-			return (0);
+	if (c > 47 && c < 58)
+		return (1);
+
+	return (0);
 }
