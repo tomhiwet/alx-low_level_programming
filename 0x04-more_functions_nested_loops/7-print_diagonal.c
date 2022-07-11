ae1689e8c75769fb8695e49dbfeb931b87cb502d
tomhiwet <tomhiwet@gmail.com>
2022-07-11 06:34:44 -0700

    fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,20 +1,29 @@
 #include "main.h"
 
 /**
- * print_line - draw a straight line
- * @n: number of times underscore is printed
+ * print_diagonal - draw a diagonal line
+ * @n: number of times the '\' char is printed
  * Description: Can only use _putchar to print
  */
-void print_line(int n)
+void print_diagonal(int n)
 {
-	int c;
+	int c, i;
 
 	c = 0;
-	while (c < n)
-	{
-		_putchar('_');
 
+	while (n > 0)
+	{
+		i = c;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		_putchar('\\');
+		_putchar('\n');
 		c++;
+		n--;
 	}
-	_putchar('\n');
+	if (c < 1)
+		_putchar('\n');
 }
