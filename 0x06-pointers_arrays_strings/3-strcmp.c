52533caa08666e69df8d9b73370952b5176efc8b
tomhiwet <tomhiwet@gmail.com>
2022-07-19 06:53:38 -0700

    more array

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares pointers
+ * @s1: first
+ * @s2: second
+ *
+ * Return: Some
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
